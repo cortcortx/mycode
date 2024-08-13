3c946b2d35aec85692d261b1198c62a672267e1c
Cort B <cortneyb@tlgcohort.com>
2024-08-13 14:17:53 +0000

    tues warmup

---
+++ bustedcode.py
@@ -0,0 +1,9 @@
+
+#!/usr/env python
+
+def calculator():
+   num1= input("Enter your first number: ")
+   num2= input("Enter your second number: ")
+   print(num1 + num2 ==  num1 + num2)
+
+calculator()
