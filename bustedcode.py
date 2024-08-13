ae8959a25d33dc9209bfda36944d484e6d8b74ec
Cort B <cortneyb@tlgcohort.com>
2024-08-13 14:53:34 +0000

    handled errors in warmup

--- bustedcode.py
+++ bustedcode.py
@@ -1,9 +1,11 @@
 
-#!/usr/env python
+#!/usr/env python3
 
 def calculator():
    num1= input("Enter your first number: ")
    num2= input("Enter your second number: ")
-   print(num1 + num2 ==  num1 + num2)
+   num3= int(num1) + int(num2)
+
+   print(f"{num1} + {num2} = ", num3)
 
 calculator()
