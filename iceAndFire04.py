707b53c63e5824ef120c74e597dd68b6e0a28492
Cort B <cortneyb@tlgcohort.com>
2024-08-16 19:30:39 +0000

    API requests and threads

--- iceAndFire04.py
+++ iceAndFire04.py
@@ -1,24 +1,78 @@
-#!/usr/bin/python3
-"""Alta3 Research - Exploring OpenAPIs with requests"""
-# documentation for this API is at
-# https://anapioficeandfire.com/Documentation
-
 import requests
 import pprint
 
 AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
 
+"""
+• print the character's allegiances... if they have any
+"""
+
 def main():
-        ## Ask user for input
         got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! " )
+        data = requests.get(AOIF_CHAR + got_charToLookup).json()
 
-        ## Send HTTPS GET to the API of ICE and Fire character resource
-        gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+        if data["name"]:
+            name = data["name"]
+        else:
+            name = data["aliases"][0]
 
-        ## Decode the response
-        got_dj = gotresp.json()
-        pprint.pprint(got_dj)
+        books = data["books"]
+        povbooks = data["povBooks"]
+        all_books = books + povbooks
+
+        # what to do with list of links to books?
+        book_titles = []
+        house_titles = []
+        # loop over book urls; grab name of book and add to list
+        for book_url in all_books:
+            bookdata = requests.get(book_url).json()
+            bookname = bookdata["name"]
+            book_titles.append(bookname)
+        # loop over allegiance urls; grab name of house and add to list
+        for house_url in data["allegiances"]:
+            housedata = requests.get(house_url).json()
+            housename = housedata["name"]
+            house_titles.append(housename)
+
+        print(f"NAME: {name}")
+        print("BOOKS:")
+        for book in book_titles:
+            print(f"• {book}")
+
+        if house_titles: # any houses in this list?
+            print("HOUSES:")
+            for house in house_titles:
+               print(f"• {house}")
 
 if __name__ == "__main__":
         main()
+ 
+
+
+
+
+
+
+
+
+
+       
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
